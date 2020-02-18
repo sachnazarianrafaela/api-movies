19787f7b6d9a419958014d858dd477a743af4393
RAFAELA <sachnazarian@gmail.com>
2020-02-18 20:11:57 +0000

    Updates

---
+++ precache-manifest.c3d485a8117262e56df4489ca1c1a98e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143aaf911ff8614f6422844e3097dc27",
+    "url": "/reactone/index.html"
+  },
+  {
+    "revision": "3f5e1230d4414975d87d",
+    "url": "/reactone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3455fd0d5ecead8aee19",
+    "url": "/reactone/static/js/2.9c189a81.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactone/static/js/2.9c189a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5e1230d4414975d87d",
+    "url": "/reactone/static/js/main.f4a1d85a.chunk.js"
+  },
+  {
+    "revision": "96dac1c1eae5c9ad1259",
+    "url": "/reactone/static/js/runtime-main.4ad58568.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactone/static/media/logo.5d5d9eef.svg"
+  }
+]);
